ee174a2d2bdc227e2b366bb3469a3368fd7506b3
James McManamey <jfmcmanamey@gmail.com>
2019-05-14 13:19:12 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -9,7 +9,26 @@ import Editor from './components/Editor'
 import Preview from './components/Preview'
 
 
-function App() {
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      input: "# MD Previewer!\n ## Built with...\n![React](/img/reactLogo.png)  ![Bootstrap Logo](/img/BoostrapLogo.png)    ![Styled Components Logo](/img/StyledComponentsLogo.png)\n      \nInline code with backticks, \n      `<div></div>` \n\nMulti-line code with triple backticks\n```\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == ) {\n    return multiLineCode;\n  }\n}\n```  \nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\n      \n > Block Quotes...\n\nSpecial thanks to: \n- [Marked.js](https://github.com/markedjs/marked)\n- [Free Code Camp](https://freecodecamp.org)"
+    }
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+  handleChange(e){
+  
+    this.setState({
+      input: e.target.value
+    }
+    )
+  }
+
+render() {
   return (
     <Container >
   
@@ -17,14 +36,15 @@ function App() {
   
     <Row> 
       <Col >
-    <Editor />
+    <Editor input = {this.state.input} onChange = {this.handleChange}/>
     </Col>
     <Col>
-   <Preview />
+   <Preview input = {this.state.input} />
     </Col>
     </Row>
     </Container>
   );
+}
 }
 
 export default App;
