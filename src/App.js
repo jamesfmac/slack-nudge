72e693954aae3cc32c766b06dca62024372698bf
James McManamey <jfmcmanamey@gmail.com>
2019-05-14 00:10:59 -0700

    2 test passing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import Header from './components/Header' 
+import Editor from './components/Editor' 
+import Preview from './components/Preview'
+
 
 function App() {
   return (
-    <div>
+    <Container >
+  
     <Header />
-    <Button variant = 'primary'>Submit</Button>
-    </div>
+  
+    <Row> 
+      <Col >
+    <Editor />
+    </Col>
+    <Col>
+   <Preview />
+    </Col>
+    </Row>
+    </Container>
   );
 }
 
